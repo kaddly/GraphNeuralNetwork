3b97812c4fd99236e2861103f26f661a2a9ddc89
cxz <1838038611@qq.com>
2022-05-21 10:12:59 +0800

    update

---
+++ GraphEmbedding/DeepWalk/data_utils.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+import os
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def plot_graph(data_dir):
+    G = nx.read_edgelist(data_dir, create_using=nx.DiGraph(), nodetype=None, data=[('weight', int)])
+    nx.draw(G, node_size=10, font_size=10, font_color="blue", font_weight="bold")
+    plt.show()
+
+
+plot_graph('../data/wiki/Wiki_edgelist.txt')
