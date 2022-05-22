7b0df377269a7cfafdc0c43ae85c26e625dabb81
cxz <1838038611@qq.com>
2022-05-22 14:19:21 +0800

    update

---
+++ Basis/networkx_study.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+edges = pd.DataFrame()
+edges['sources'] = [0, 1, 2, 3, 4, 4, 6, 7, 7, 9, 1, 4, 4, 4, 6, 7, 5, 8, 9, 8]
+edges['targets'] = [1, 4, 4, 4, 6, 7, 5, 8, 9, 8, 0, 1, 2, 3, 4, 4, 6, 7, 7, 9]
+
+G = nx.from_pandas_edgelist(edges, source='sources', target='targets')
+nx.draw(G, node_size=10, font_size=10, font_color="blue", font_weight="bold", with_labels=True)
+plt.show()
+# degree
+print(nx.degree(G))
+# 连通分量
+print(list(nx.connected_components(G)))
+# 图直径
+print(nx.diameter(G))
+# 度中心性
+print('度中心性', nx.degree_centrality(G))
+# 特征向量中心性
+print('特征向量中心性', nx.eigenvector_centrality(G))
+# betweenNess
+print('betweenNess', nx.betweenness_centrality(G))
+# closeness
+print('closeness', nx.closeness_centrality(G))
+# pagerank
+print('pagerank', nx.pagerank(G))
+# HITS
+print('HITS', nx.hits(G, tol=0.00001))
