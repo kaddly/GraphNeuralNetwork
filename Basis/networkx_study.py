d64f2388f62f5d0e2f5d3676a1f6e00cfc49218e
cxz <1838038611@qq.com>
2022-05-22 14:20:20 +0800

    update

--- Basis/networkx_study.py
+++ Basis/networkx_study.py
@@ -7,8 +7,7 @@ edges['sources'] = [0, 1, 2, 3, 4, 4, 6, 7, 7, 9, 1, 4, 4, 4, 6, 7, 5, 8, 9, 8]
 edges['targets'] = [1, 4, 4, 4, 6, 7, 5, 8, 9, 8, 0, 1, 2, 3, 4, 4, 6, 7, 7, 9]
 
 G = nx.from_pandas_edgelist(edges, source='sources', target='targets')
-nx.draw(G, node_size=10, font_size=10, font_color="blue", font_weight="bold", with_labels=True)
-plt.show()
+
 # degree
 print(nx.degree(G))
 # 连通分量
@@ -27,3 +26,6 @@ print('closeness', nx.closeness_centrality(G))
 print('pagerank', nx.pagerank(G))
 # HITS
 print('HITS', nx.hits(G, tol=0.00001))
+# 绘制图
+nx.draw(G, node_size=10, font_size=10, font_color="blue", font_weight="bold", with_labels=True)
+plt.show()
