a511ef4c5c1ff3ca2b38b72444f57799cd8f2de6
cxz <1838038611@qq.com>
2022-05-23 14:42:54 +0800

    update

--- GraphEmbedding/DeepWalk/DeepWalk.py
+++ GraphEmbedding/DeepWalk/DeepWalk.py
@@ -2,3 +2,16 @@ import torch
 from torch import nn
 
 
+def skip_gram(center, contexts_and_negatives, embed_v, embed_u):
+    v = embed_v(center)
+    u = embed_u(contexts_and_negatives)
+    pred = torch.bmm(v, u.permute(0, 2, 1))
+    return pred
+
+
+class Word2vec:
+    def __init__(self, vocab_size, embed_size):
+        self.net = nn.Sequential(nn.Embedding(vocab_size, embed_size), nn.Embedding(vocab_size, embed_size))
+
+    def __call__(self, center, context_negative):
+        return skip_gram(center, context_negative, self.net[0], self.net[1])
