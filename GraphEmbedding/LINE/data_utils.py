58ccab3a4ec14c319af36cad9571dc331215f874
cxz <1838038611@qq.com>
2022-05-27 10:01:08 +0800

    update

--- GraphEmbedding/LINE/data_utils.py
+++ GraphEmbedding/LINE/data_utils.py
@@ -1,10 +1,22 @@
 import os
 import torch
 import networkx as nx
+from graph_utils import preprocess_nxgraph
 
 
 def read_wiki(data_dir):
-    return nx.read_edgelist(data_dir, create_using=nx.DiGraph())
+    return nx.read_edgelist(data_dir, create_using=nx.Graph())
 
 
+def gen_sample_data(G, power):
+    idx2node, node2idx = preprocess_nxgraph(G)
+    node_size = G.number_of_nodes()
+    node_degree = torch.zeros(node_size)
+    for edge in G.edges():
+        node_degree[node2idx[edge[0]]] += G[edge[0]][edge[1]].get('weight', 1.0)
+    total_sum = torch.sum(torch.Tensor([torch.pow(node_degree[i], power) for i in range(node_size)]))
+    norm_prob = torch.Tensor([float(torch.pow(node_degree[j], power)) / total_sum for j in range(node_size)])
+    print(norm_prob)
 
+
+gen_sample_data(read_wiki("../data/wiki/Wiki_edgelist.txt"), 0.75)
