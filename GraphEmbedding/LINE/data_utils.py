073b010ec5a66b913a379fb14563bf9f367f4a35
cxz <1838038611@qq.com>
2022-05-28 10:30:13 +0800

    update

--- GraphEmbedding/LINE/data_utils.py
+++ GraphEmbedding/LINE/data_utils.py
@@ -9,6 +9,25 @@ def read_wiki(data_dir):
     return nx.read_edgelist(data_dir, create_using=nx.Graph())
 
 
+class RandomGenerator:
+    """根据n个采样权重在{1,...,n}中随机抽取"""
+
+    def __init__(self, sampling_weights):
+        # Exclude
+        self.population = list(range(1, len(sampling_weights) + 1))
+        self.sampling_weights = sampling_weights
+        self.candidates = []
+        self.i = 0
+
+    def draw(self):
+        if self.i == len(self.candidates):
+            # 缓存k个随机采样结果
+            self.candidates = random.choices(self.population, self.sampling_weights, k=10000)
+            self.i = 0
+        self.i += 1
+        return self.candidates[self.i - 1]
+
+
 def gen_sample_data(G, power):
     idx2node, node2idx = preprocess_nxgraph(G)
     node_size = G.number_of_nodes()
@@ -17,6 +36,7 @@ def gen_sample_data(G, power):
         node_degree[node2idx[edge[0]]] += G[edge[0]][edge[1]].get('weight', 1.0)
     total_sum = torch.sum(torch.Tensor([torch.pow(node_degree[i], power) for i in range(node_size)]))
     norm_prob = torch.Tensor([torch.pow(node_degree[j], power) / total_sum for j in range(node_size)])
+    generator = RandomGenerator(norm_prob)
 
 
 
