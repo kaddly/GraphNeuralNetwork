f61a6e8b59b25da0aa1424df2fc23e4dcc390845
cxz <1838038611@qq.com>
2022-05-29 10:04:58 +0800

    update

--- GraphEmbedding/LINE/data_utils.py
+++ GraphEmbedding/LINE/data_utils.py
@@ -8,7 +8,7 @@ from graph_utils import preprocess_nxgraph
 
 
 def read_wiki(data_dir):
-    return nx.read_edgelist(data_dir, create_using=nx.Graph())
+    return nx.read_edgelist(data_dir, create_using=nx.Graph(), )
 
 
 class RandomGenerator:
@@ -16,7 +16,7 @@ class RandomGenerator:
 
     def __init__(self, sampling_weights):
         # Exclude
-        self.population = list(range(1, len(sampling_weights) + 1))
+        self.population = list(range(0, len(sampling_weights)))
         self.sampling_weights = sampling_weights
         self.candidates = []
         self.i = 0
@@ -30,6 +30,14 @@ class RandomGenerator:
         return self.candidates[self.i - 1]
 
 
+def get_centers_and_contexts(G):
+    nodes, contexts = [], []
+    for node in G.nodes():
+        nodes.append(int(node))
+        contexts.append(list(map(int, G.neighbors(node))))
+    return nodes, contexts
+
+
 def get_sampling_weights(G, node2idx, power):
     node_size = G.number_of_nodes()
     node_degree = np.zeros(node_size)
@@ -41,16 +49,57 @@ def get_sampling_weights(G, node2idx, power):
     return norm_prob
 
 
+def get_negative(G, all_contexts, idx2node, node2idx, K):
+    sampling_weights = get_sampling_weights(G, node2idx, 0.75)
+    all_negatives = []
+    generator = RandomGenerator(sampling_weights)
+    for contexts in all_contexts:
+        negatives = []
+        while len(negatives) < len(contexts) * K:
+            neg = generator.draw()
+            # 噪声词不能是上下⽂词
+            if idx2node[neg] not in contexts:
+                negatives.append(idx2node[neg])
+        all_negatives.append(negatives)
+    return all_negatives
+
+
+def batchify(data):
+    """返回带有负采样的跳元模型的⼩批量样本"""
+    max_len = max(len(c) + len(n) for _, c, n in data)
+    print(max_len)
+    centers, contexts_negatives, masks, labels = [], [], [], []
+    for center, context, negative in data:
+        cur_len = len(context) + len(negative)
+        centers += [center]
+        contexts_negatives += [context + negative + [0] * (max_len - cur_len)]
+        masks += [[1] * cur_len + [0] * (max_len - cur_len)]
+        labels += [[1] * len(context) + [0] * (max_len - len(context))]
+    return (torch.tensor(centers).reshape((-1, 1))
+            , torch.tensor(contexts_negatives)
+            , torch.tensor(masks)
+            , torch.tensor(labels))
+
+
 class Wiki_dataset(Dataset):
-    def __init__(self, G):
-        self.G = G
-        self.idx2node, self.node2idx = preprocess_nxgraph(G)
+    def __init__(self, centers, contexts, negatives):
+        assert len(centers) == len(contexts) == len(negatives)
+        self.centers = centers
+        self.contexts = contexts
+        self.negatives = negatives
 
     def __getitem__(self, item):
-        pass
+        return self.centers[item], self.negatives[item], self.contexts[item]
 
     def __len__(self):
-        pass
+        return len(self.centers)
 
 
-print(sum(get_sampling_weights(read_wiki("../data/wiki/Wiki_edgelist.txt"), 0.75)))
+def load_data_wiki(data_dir, batch_size, num_noise_words):
+    G = read_wiki(data_dir)
+    idx2node, node2idx = preprocess_nxgraph(G)
+    nodes, all_contexts = get_centers_and_contexts(G)
+    all_negatives = get_negative(G, all_contexts, idx2node, node2idx, num_noise_words)
+    dataset = Wiki_dataset(nodes, all_contexts, all_negatives)
+    data_iter = DataLoader(dataset, batch_size, shuffle=True, collate_fn=batchify)
+    return data_iter
--- GraphEmbedding/LINE/graph_utils.py
+++ GraphEmbedding/LINE/graph_utils.py
@@ -4,7 +4,7 @@ def preprocess_nxgraph(graph):
     node_size = 0
     for node in graph.nodes():
         node2idx[node] = node_size
-        idx2node.append(node)
+        idx2node.append(int(node))
         node_size += 1
     return idx2node, node2idx
 
