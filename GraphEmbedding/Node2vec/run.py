6a504c6fb36cf4602bf3c2ecf439cf2ff225e44d
cxz <1838038611@qq.com>
2022-06-01 16:30:56 +0800

    update

--- GraphEmbedding/Node2vec/run.py
+++ GraphEmbedding/Node2vec/run.py
@@ -4,10 +4,10 @@ from utils.data_utils import load_flight_data
 from train_eval import train, get_similar_tokens
 
 if __name__ == '__main__':
-    batch_size, max_window_size, num_noise_words = 512, 5, 5
+    batch_size, max_window_size, num_noise_words = 32, 5, 5
     lr, num_epochs, device = 0.002, 5, torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data_iter, idx2node, node2idx = load_flight_data('../data/flight/brazil-airports.edgelist',
-                                                     batch_size=batch_size, num_walks=16, walk_length=10, workers=4,
+                                                     batch_size=batch_size, num_walks=16, walk_length=8, workers=4,
                                                      max_window_size=max_window_size, num_noise_words=num_noise_words)
     vocab_size, embed_size = len(node2idx), 100
     model = Word2vec(vocab_size, embed_size)
