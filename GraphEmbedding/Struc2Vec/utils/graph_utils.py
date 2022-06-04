752fcbef7e991d57e5e0c22bb6a4b219e2c22258
cxz <1838038611@qq.com>
2022-06-04 10:08:40 +0800

    update

--- GraphEmbedding/Struc2Vec/utils/graph_utils.py
+++ GraphEmbedding/Struc2Vec/utils/graph_utils.py
@@ -1,6 +1,26 @@
 import collections
-from collections import deque
+from collections import ChainMap, deque
 from .fastdtw import fastdtw
+from .sample_utils import create_alias_table
+import numpy as np
+import math
+from joblib import Parallel, delayed
+
+
+def partition_dict(vertices, workers):
+    batch_size = (len(vertices) - 1) // workers + 1
+    part_list = []
+    part = []
+    count = 0
+    for v1, nbs in vertices.items():
+        part.append((v1, nbs))
+        count += 1
+        if count % batch_size == 0:
+            part_list.append(part)
+            part = []
+    if len(part) > 0:
+        part_list.append(part)
+    return part_list
 
 
 def preprocess_nxgraph(graph):
@@ -66,7 +86,8 @@ def _compute_ordered_degree_list(graph, idx2node, node2idx, opt1_reduce_len=True
     degreeList = {}
     vertices = list(range(len(idx2node)))
     for v in vertices:
-        degreeList[v] = _get_order_degree_list_node(graph, idx2node, node2idx, v, opt1_reduce_len=opt1_reduce_len, max_num_layers=max_num_layers)
+        degreeList[v] = _get_order_degree_list_node(graph, idx2node, node2idx, v, opt1_reduce_len=opt1_reduce_len,
+                                                    max_num_layers=max_num_layers)
     return degreeList
 
 
@@ -79,14 +100,131 @@ def compute_dtw_dist(part_list, degreeList, dist_func):
             max_layer = min(len(lists_v1), len(lists_v2))  # valid layer
             dtw_dist[v1, v2] = {}
             for layer in range(0, max_layer):
-                dist, path = fastdtw(
-                    lists_v1[layer], lists_v2[layer], radius=1, dist=dist_func)
+                dist, path = fastdtw(lists_v1[layer], lists_v2[layer], radius=1, dist=dist_func)
                 dtw_dist[v1, v2][layer] = dist
     return dtw_dist
 
 
-def _compute_structural_distance(self, max_num_layers, workers=1, verbose=0):
-    pass
+def _compute_structural_distance(graph, idx2node, node2idx, opt1_reduce_len=True, opt2_reduce_sim_calc=True,
+                                 max_num_layers=None, workers=1,
+                                 verbose=0):
+    if opt1_reduce_len:
+        dist_func = cost_max
+    else:
+        dist_func = cost
+    degreeList = _compute_ordered_degree_list(graph, idx2node, node2idx, opt1_reduce_len, max_num_layers)
+    if opt2_reduce_sim_calc:
+        degrees = _create_vectors(graph, idx2node)
+        degreeListsSelected = {}
+        vertices = {}
+        n_nodes = len(idx2node)
+        for v in range(n_nodes):  # c:list of vertex
+            nbs = get_vertices(
+                v, len(graph[idx2node[v]]), degrees, n_nodes)
+            vertices[v] = nbs  # store nbs
+            degreeListsSelected[v] = degreeList[v]  # store dist
+            for n in nbs:
+                # store dist of nbs
+                degreeListsSelected[n] = degreeList[n]
+    else:
+        vertices = {}
+        for v in degreeList:
+            vertices[v] = [vd for vd in degreeList.keys() if vd > v]
+    results = Parallel(n_jobs=workers, verbose=verbose, )(
+        delayed(compute_dtw_dist)(part_list, degreeList, dist_func) for part_list in partition_dict(vertices, workers))
+    dtw_dist = dict(ChainMap(*results))
+    structural_dist = convert_dtw_struc_dist(dtw_dist)
+
+    return structural_dist
+
+
+def _create_vectors(G, idx2node):
+    degrees = {}  # sotre v list of degree
+    degrees_sorted = set()  # store degree
+    for v in range(len(idx2node)):
+        degree = len(G[idx2node[v]])
+        degrees_sorted.add(degree)
+        if degree not in degrees:
+            degrees[degree] = {}
+            degrees[degree]['vertices'] = []
+        degrees[degree]['vertices'].append(v)
+    degrees_sorted = np.array(list(degrees_sorted), dtype='int')
+    degrees_sorted = np.sort(degrees_sorted)
+
+    l = len(degrees_sorted)
+    for index, degree in enumerate(degrees_sorted):
+        if index > 0:
+            degrees[degree]['before'] = degrees_sorted[index - 1]
+        if index < (l - 1):
+            degrees[degree]['after'] = degrees_sorted[index + 1]
+
+    return degrees
+
+
+def _get_transition_probs(layers_adj, layers_distances):
+    layers_alias = {}
+    layers_accept = {}
+
+    for layer in layers_adj:
+
+        neighbors = layers_adj[layer]
+        layer_distances = layers_distances[layer]
+        node_alias_dict = {}
+        node_accept_dict = {}
+        norm_weights = {}
+
+        for v, neighbors in neighbors.items():
+            e_list = []
+            sum_w = 0.0
+
+            for n in neighbors:
+                if (v, n) in layer_distances:
+                    wd = layer_distances[v, n]
+                else:
+                    wd = layer_distances[n, v]
+                w = np.exp(-float(wd))
+                e_list.append(w)
+                sum_w += w
+
+            e_list = [x / sum_w for x in e_list]
+            norm_weights[v] = e_list
+            accept, alias = create_alias_table(e_list)
+            node_alias_dict[v] = alias
+            node_accept_dict[v] = accept
+
+        layers_alias[layer] = node_alias_dict
+        layers_accept[layer] = node_accept_dict
+
+    return layers_accept, layers_alias, norm_weights
+
+
+def prepare_biased_walk(norm_weights):
+    sum_weights = {}
+    sum_edges = {}
+    average_weight = {}
+    gamma = {}
+    layer = 0
+    while norm_weights:
+        probs = norm_weights
+        for v, list_weights in probs.items():
+            sum_weights.setdefault(layer, 0)
+            sum_edges.setdefault(layer, 0)
+            sum_weights[layer] += sum(list_weights)
+            sum_edges[layer] += len(list_weights)
+
+        average_weight[layer] = sum_weights[layer] / sum_edges[layer]
+
+        gamma.setdefault(layer, {})
+
+        for v, list_weights in probs.items():
+            num_neighbours = 0
+            for w in list_weights:
+                if w > average_weight[layer]:
+                    num_neighbours += 1
+            gamma[layer][v] = num_neighbours
+
+        layer += 1
+    return average_weight, gamma
 
 
 def cost(a, b):
@@ -108,3 +246,87 @@ def cost_max(a, b):
     m = max(a[0], b[0]) + ep
     mi = min(a[0], b[0]) + ep
     return ((m / mi) - 1) * max(a[1], b[1])
+
+
+def convert_dtw_struc_dist(distances, startLayer=1):
+    """
+
+    :param distances: dict of dict
+    :param startLayer:
+    :return:
+    """
+    for vertices, layers in distances.items():
+        keys_layers = sorted(layers.keys())
+        startLayer = min(len(keys_layers), startLayer)
+        for layer in range(0, startLayer):
+            keys_layers.pop(0)
+
+        for layer in keys_layers:
+            layers[layer] += layers[layer - 1]
+    return distances
+
+
+def get_vertices(v, degree_v, degrees, n_nodes):
+    a_vertices_selected = 2 * math.log(n_nodes, 2)
+    vertices = []
+    try:
+        c_v = 0
+
+        for v2 in degrees[degree_v]['vertices']:
+            if v != v2:
+                vertices.append(v2)  # same degree
+                c_v += 1
+                if c_v > a_vertices_selected:
+                    raise StopIteration
+
+        if 'before' not in degrees[degree_v]:
+            degree_b = -1
+        else:
+            degree_b = degrees[degree_v]['before']
+        if 'after' not in degrees[degree_v]:
+            degree_a = -1
+        else:
+            degree_a = degrees[degree_v]['after']
+        if degree_b == -1 and degree_a == -1:
+            raise StopIteration  # not anymore v
+        degree_now = verifyDegrees(degree_v, degree_a, degree_b)
+        # nearest valid degree
+        while True:
+            for v2 in degrees[degree_now]['vertices']:
+                if v != v2:
+                    vertices.append(v2)
+                    c_v += 1
+                    if c_v > a_vertices_selected:
+                        raise StopIteration
+
+            if degree_now == degree_b:
+                if 'before' not in degrees[degree_b]:
+                    degree_b = -1
+                else:
+                    degree_b = degrees[degree_b]['before']
+            else:
+                if 'after' not in degrees[degree_a]:
+                    degree_a = -1
+                else:
+                    degree_a = degrees[degree_a]['after']
+
+            if degree_b == -1 and degree_a == -1:
+                raise StopIteration
+
+            degree_now = verifyDegrees(degree_v, degree_a, degree_b)
+
+    except StopIteration:
+        return list(vertices)
+
+
+def verifyDegrees(degree_v_root, degree_a, degree_b):
+    if degree_b == -1:
+        degree_now = degree_a
+    elif degree_a == -1:
+        degree_now = degree_b
+    elif abs(degree_b - degree_v_root) < abs(degree_a - degree_v_root):
+        degree_now = degree_b
+    else:
+        degree_now = degree_a
+
+    return degree_now
