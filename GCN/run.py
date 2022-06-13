7c2035c1d75c7c65475775d94cb2f6190dab01f8
cxz <1838038611@qq.com>
2022-06-13 12:08:31 +0800

    update

---
+++ GCN/run.py
@@ -0,0 +1,7 @@
+import torch
+from .data_utils import load_cora
+from .GCN import GCN_Model
+from .train_eval import train, test
+
+if __name__ == '__main__':
+    pass
---
+++ GCN/train_eval.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+import time
+import os
+from datetime import timedelta
+
+
+def train():
+    pass
+
+
+def test():
+    pass
