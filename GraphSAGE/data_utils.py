c22e2537d10e3ff2324b31187738210637ce38fa
cxz <1838038611@qq.com>
2022-06-16 09:44:24 +0800

    update

--- GraphSAGE/data_utils.py
+++ GraphSAGE/data_utils.py
@@ -1,5 +1,6 @@
 import os
 from collections import defaultdict
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 
@@ -19,7 +20,7 @@ def read_data(data_dir):
             info = line.split("\t")
             node_map[info[0]] = i
             labels.append(int(info[1].split("=")[1]) - 1)
-            tmp_list = [0]*(len(feat_map) - 2)
+            tmp_list = [0] * (len(feat_map) - 2)
             for word_info in info[2:-1]:
                 word_info = word_info.split("=")
                 tmp_list[feat_map[word_info[0]]] = float(word_info[1])
@@ -37,3 +38,34 @@ def read_data(data_dir):
             adj_lists[paper2].add(paper1)
     assert len(feat_data) == len(labels) == len(adj_lists)
     return feat_data, labels, adj_lists
+
+
+def train_test_split(node_nums, test_split=0.3, val_split=0.6):
+    rand_indices = list(range(node_nums))
+
+    test_size = int(node_nums * test_split)
+    val_size = int(node_nums * val_split)
+    train_size = node_nums - (test_size + val_size)
+
+    test_indexs = rand_indices[:test_size]
+    val_indexs = rand_indices[test_size:(test_size + val_size)]
+    train_indexs = rand_indices[(test_size + val_size):]
+
+    return train_indexs, val_indexs, test_indexs
+
+
+class pubmed_dataset(Dataset):
+    def __init__(self, feat_data, labels, adj_lists):
+        self.feat_data = torch.Tensor(feat_data)
+        self.labels = torch.Tensor(labels)
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def load_pubmed_data(data_dir, batch_size):
+    feat_data, labels, adj_lists = read_data(data_dir)
+    print()
