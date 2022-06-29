bddd8a7cf5ebbf33860e1fb7f1397a0c4b7ff6bd
cxz <1838038611@qq.com>
2022-06-29 10:54:53 +0800

    update

--- GAT/data_utils.py
+++ GAT/data_utils.py
@@ -1,5 +1,36 @@
+import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
-import os
+import scipy.sparse as sp
 
 
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def read_cora(data_dir='./data/cora/', dataset="cora"):
+    print('Loading {} dataset...'.format(dataset))
+    idx_features_labels = np.genfromtxt("{}{}.content".format(data_dir, dataset), dtype=np.dtype(str))
+    edges_unordered = np.genfromtxt("{}{}.cites".format(data_dir, dataset), dtype=np.int32)
+    return idx_features_labels, edges_unordered
+
+
+def preprocess_node_map(idx_features_labels):
+    idx2node = np.array(idx_features_labels[:, 0], dtype=np.int32)  # 节点
+    node2idx = {j: i for i, j in enumerate(idx2node)}  # 构建节点的索引字典
+    return idx2node, node2idx
+
+
+def preprocess_data(idx_features_labels, edges_unordered, node2idx):
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)  # 取特征feature
+    labels = encode_onehot(idx_features_labels[:, -1])  # one-hot label
+    edges = np.array(list(map(node2idx.get, edges_unordered.flatten())), dtype=np.int32).reshape(
+        edges_unordered.shape)  # 将之前的转换成字典编号后的边
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)  # 构建边的邻接矩阵
+    # build symmetric adjacency matrix，计算转置矩阵。将有向图转成无向图
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    return features, labels, adj
--- GAT/models/GAT.py
+++ GAT/models/GAT.py
@@ -1,53 +1,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .layers import GraphAttentionLayer, SpGraphAttentionLayer
+from layers import GraphAttentionLayer, SpGraphAttentionLayer
 
 
-class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, **kwargs):
-        """Dense version of GAT."""
-        super(GAT, self).__init__(**kwargs)
+class GATBase(nn.Module):
+    def __init__(self, dropout, **kwargs):
+        super(GATBase, self).__init__(**kwargs)
         self.dropout = dropout
-
-        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
-        for i, attention in enumerate(self.attentions):
-            self.add_module('attention_{}'.format(i), attention)
-
-        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)  # 第二层(最后一层)的attention layer
+        self.attentions = nn.ModuleList()
+        self.out_att = None
 
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
         x = torch.cat([att(x, adj) for att in self.attentions], dim=1)  # 将每层attention拼接
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj))   # 第二层的attention layer
+        x = F.elu(self.out_att(x, adj))  # 第二层的attention layer
         return F.log_softmax(x, dim=1)
 
 
-class SpGAT(nn.Module):
+class GAT(GATBase):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, **kwargs):
+        """Dense version of GAT."""
+        super(GAT, self).__init__(dropout, **kwargs)
+        for i in nheads:
+            self.attentions.add_module(f'AttentionHead{i}',
+                                       GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True))
+        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
+
+
+class SpGAT(GATBase):
     def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, **kwargs):
         """Sparse version of GAT."""
-        super(SpGAT, self).__init__(**kwargs)
-        self.dropout = dropout
-
-        self.attentions = [SpGraphAttentionLayer(nfeat,
-                                                 nhid,
-                                                 dropout=dropout,
-                                                 alpha=alpha,
-                                                 concat=True) for _ in range(nheads)]
-        for i, attention in enumerate(self.attentions):
-            self.add_module('attention_{}'.format(i), attention)
-
-        self.out_att = SpGraphAttentionLayer(nhid * nheads,
-                                             nclass,
-                                             dropout=dropout,
-                                             alpha=alpha,
-                                             concat=False)
-
-    def forward(self, x, adj):
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj))
-        return F.log_softmax(x, dim=1)
-
+        super(SpGAT, self).__init__(dropout, **kwargs)
+        for i in nheads:
+            self.attentions.add_module(f'AttentionHead{i}',
+                                       SpGraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True))
+        self.out_att = SpGraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
