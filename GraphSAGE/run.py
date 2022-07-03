e6d85d04519f1927b697c4f5245e2ecdf0e1c891
cxz <1838038611@qq.com>
2022-07-03 09:23:46 +0800

    update

--- GraphSAGE/run.py
+++ GraphSAGE/run.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     is_gcn, is_unsupervised = False, False
     num_neighs, num_layers = 10, 2
-    batch_size, num_epochs, lr = 64, 50, 0.01
+    batch_size, num_epochs, lr = 64, 50, 0.05
     input_size, hidden_size = 500, 128
     train_iter, val_iter, test_iter = load_pubmed_data('./data/pubmed-data', batch_size, num_layers,
                                                        num_neighs, window_size=5, num_noise_words=5, is_gcn=is_gcn,
