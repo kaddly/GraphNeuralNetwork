f061502cddc77078648d14a7aafc13ad5deb971b
cxz <1838038611@qq.com>
2022-08-22 08:50:13 +0800

    update

---
+++ HAN/models/layers.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class GraphAttentionLayer(nn.Module):
+    def __init__(self, **kwargs):
+        super(GraphAttentionLayer, self).__init__(**kwargs)
+
+    def forward(self):
+        pass
