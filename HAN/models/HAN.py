73f8b9f849f7fdb34f0bf58916a1269da4de96ab
cxz <1838038611@qq.com>
2022-08-24 09:16:31 +0800

    update

--- HAN/models/HAN.py
+++ HAN/models/HAN.py
@@ -6,9 +6,12 @@ from .SemanticAttention import SemanticAttention
 
 
 class HANLayer(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, num_meta_paths, in_size, out_size, layer_num_heads, dropout, **kwargs):
         super(HANLayer, self).__init__(**kwargs)
-        nn.ModuleList()
+        self.gat_layers = nn.ModuleList()
+        for i in range(num_meta_paths):
+            self.gat_layers.add_module(f'meta_path_model{i}',
+                                       GATConv(in_size, out_size, layer_num_heads, dropout))
 
     def forward(self):
         pass
---
+++ HAN/run.py
@@ -0,0 +1,6 @@
+import torch
+
+
+if __name__ == '__main__':
+    devices = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
+    devices = devices if devices else [torch.device('cpu')]
