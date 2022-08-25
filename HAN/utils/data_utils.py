9e40eea3eb8445821390175870f80c2758b6cac6
cxz <1838038611@qq.com>
2022-08-25 09:22:23 +0800

    update

--- HAN/utils/data_utils.py
+++ HAN/utils/data_utils.py
@@ -1,6 +1,29 @@
 import os
+import random
+import pickle
+import errno
 import torch
 from torch.utils.data import DataLoader, Dataset
+
+
+def mkdir_p(path, log=True):
+    """Create a directory for the specified path.
+    Parameters
+    ----------
+    path : str
+        Path name
+    log : bool
+        Whether to print result for directory creation
+    """
+    try:
+        os.makedirs(path)
+        if log:
+            print('Created directory {}'.format(path))
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path) and log:
+            print('Directory {} already exists.'.format(path))
+        else:
+            raise
 
 
 def load_data():
