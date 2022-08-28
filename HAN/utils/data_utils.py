7083ea32c3bfdc927165f539ba41917ee9102aa8
cxz <1838038611@qq.com>
2022-08-28 09:52:55 +0800

    update

--- HAN/utils/data_utils.py
+++ HAN/utils/data_utils.py
@@ -29,9 +29,47 @@ def mkdir_p(path, log=True):
 
 
 def load_data(data_dir='../data/ACM.mat'):
+    """
+    异构数据处理
+    :param data_dir:
+    :return:
+    """
     matHG = sio.loadmat(data_dir)
-    keys = matHG.keys()
-    print(keys)
+    p_vs_l = matHG['PvsL']       # paper-field?
+    p_vs_a = matHG['PvsA']       # paper-author
+    p_vs_t = matHG['PvsT']       # paper-term, bag of words
+    p_vs_c = matHG['PvsC']       # paper-conference, labels come from that
+
+    # We assign
+    # (1) KDD papers as class 0 (data mining),
+    # (2) SIGMOD and VLDB papers as class 1 (database),
+    # (3) SIGCOMM and MOBICOMM papers as class 2 (communication)
+    conf_ids = [0, 1, 9, 10, 13]
+    label_ids = [0, 1, 2, 2, 1]
+    '''
+    array([[array(['KDD'], dtype='<U3')],
+       [array(['SIGMOD'], dtype='<U6')],
+       [array(['WWW'], dtype='<U3')],
+       [array(['SIGIR'], dtype='<U5')],
+       [array(['CIKM'], dtype='<U4')],
+       [array(['SODA'], dtype='<U4')],
+       [array(['STOC'], dtype='<U4')],
+       [array(['SOSP'], dtype='<U4')],
+       [array(['SPAA'], dtype='<U4')],
+       [array(['SIGCOMM'], dtype='<U7')],
+       [array(['MobiCOMM'], dtype='<U8')],
+       [array(['ICML'], dtype='<U4')],
+       [array(['COLT'], dtype='<U4')],
+       [array(['VLDB'], dtype='<U4')]], dtype=object)
+    '''
+    p_vs_c_filter = p_vs_c[:, conf_ids]
+    p_selected = (p_vs_c_filter.sum(1) != 0).A1.nonzero()[0]
+    p_vs_l = p_vs_l[p_selected]
+    p_vs_a = p_vs_a[p_selected]
+    p_vs_t = p_vs_t[p_selected]
+    p_vs_c = p_vs_c[p_selected]
+    feature = p_vs_t.toarray()
+    return
 
 
 class Heterograph:
