abd5d3b142e317744c46fddac7999d3e4009564e
cxz <1838038611@qq.com>
2022-09-04 08:07:27 +0800

    update

---
+++ GTN/run.py
@@ -0,0 +1,5 @@
+import torch
+
+if __name__ == '__main__':
+    devices = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
+    devices = devices if devices else [torch.device('cpu')]
