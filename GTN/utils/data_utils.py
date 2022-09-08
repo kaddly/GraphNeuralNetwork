d74d1c324ebfd5b8c58f55c1deac645aa43b6a5b
cxz <1838038611@qq.com>
2022-09-08 08:41:57 +0800

    update

--- GTN/utils/data_utils.py
+++ GTN/utils/data_utils.py
@@ -8,6 +8,11 @@ from torch.utils.data import DataLoader, Dataset
 
 
 def read_acm(data_dir='../data/acm.mat'):
+    """
+    读取mat数据
+    :param data_dir:
+    :return:
+    """
     return sio.loadmat(data_dir)
 
 
@@ -39,9 +44,9 @@ def process_paper_target(mat_file):
     return paper_idx, paper_target
 
 
-def process_edge(mat_file, paper_idx):
+def process_edge_feature(mat_file, paper_idx):
     """
-    处理邻接矩阵
+    处理邻接矩阵以及特征
     :param mat_file:
     :param paper_idx:
     :return:
@@ -77,10 +82,60 @@ def process_edge(mat_file, paper_idx):
     A_ps = csr_matrix((data, (papers, re_subjects)), shape=(node_num, node_num))
     A_sp = A_ps.transpose()
 
-    return [A_pa, A_ap, A_ps, A_sp]
+    terms = mat_file['TvsP'].transpose()[paper_idx].nonzero()[1]
+    term_dic = {}
+    re_terms = []
+    for term in terms:
+        if term not in term_dic:
+            term_dic[term] = len(term_dic) + len(paper_idx) + len(author_dic) + len(subject_dic)
+        re_terms.append(term_dic[term])
+    re_terms = np.array(re_terms)
+    # tmp
+    tmp_num_node = node_num + len(term_dic)
+    papers = mat_file['PvsA'][paper_idx].nonzero()[0]
+    data = np.ones_like(papers)
+    A_pa_tmp = csr_matrix((data, (papers, re_authors)), shape=(tmp_num_node, tmp_num_node))
+    papers = mat_file['PvsL'][paper_idx].nonzero()[0]
+    data = np.ones_like(papers)
+    A_ps_tmp = csr_matrix((data, (papers, re_subjects)), shape=(tmp_num_node, tmp_num_node))
+    papers = mat_file['PvsT'][paper_idx].nonzero()[0]
+    data = np.ones_like(papers)
+    A_pt_tmp = csr_matrix((data, (papers, re_terms)), shape=(tmp_num_node, tmp_num_node))
+    paper_feat = np.array(A_pt_tmp[:len(paper_idx), -len(term_dic):].toarray() > 0, dtype=np.int)
+    author_feat = np.array(A_pa_tmp.transpose().dot(A_pt_tmp)[len(paper_idx):len(paper_idx) + len(author_dic),
+                           -len(term_dic):].toarray() > 0, dtype=np.int)
+    subject_feat = np.array(A_ps_tmp.transpose().dot(A_pt_tmp)[
+                            len(paper_idx) + len(author_dic):len(paper_idx) + len(author_dic) + len(subject_dic),
+                            -len(term_dic):].toarray() > 0, dtype=np.int)
+    node_feature = np.concatenate((paper_feat, author_feat, subject_feat))
+    return [A_pa, A_ap, A_ps, A_sp], node_feature
+
+
+def train_test_split(paper_target):
+    # Train, Valid
+    train_valid_DB = list(np.random.choice(np.where(paper_target == 0)[0], 300, replace=False))
+    train_valid_WC = list(np.random.choice(np.where(paper_target == 1)[0], 300, replace=False))
+    train_valid_DM = list(np.random.choice(np.where(paper_target == 2)[0], 300, replace=False))
+
+    train_idx = np.array(train_valid_DB[:200] + train_valid_WC[:200] + train_valid_DM[:200])
+    train_target = paper_target[train_idx]
+    train_label = np.vstack((train_idx, train_target)).transpose()
+    valid_idx = np.array(train_valid_DB[200:] + train_valid_WC[200:] + train_valid_DM[200:])
+    valid_target = paper_target[valid_idx]
+    valid_label = np.vstack((valid_idx, valid_target)).transpose()
+    test_idx = np.array(list((set(np.arange(paper_target.shape[0])) - set(train_idx)) - set(valid_idx)))
+    test_target = paper_target[test_idx]
+    test_label = np.vstack((test_idx, test_target)).transpose()
+    labels = [train_label, valid_label, test_label]
+    return labels
 
 
 def load_acm():
     mat_file = read_acm()
     paper_idx, paper_target = process_paper_target(mat_file)
-    edges = process_edge(mat_file, paper_idx)
+    edges, node_feature = process_edge_feature(mat_file, paper_idx)
+    labels = train_test_split(paper_target)
+    return edges, node_feature, labels
+
+
+load_acm()
