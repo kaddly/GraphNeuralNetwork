1c8460cddcbd7d1406ffb338364143b9d186ddf9
cxz <1838038611@qq.com>
2022-09-11 08:46:15 +0800

    update

--- GTN/run.py
+++ GTN/run.py
@@ -1,4 +1,4 @@
-import torch
+from utils import load_acm
 
 
 def parse_args():
@@ -8,15 +8,18 @@ def parse_args():
     parser.add_argument('--dataset', type=str, help='Dataset')
     parser.add_argument('--epoch', type=int, default=40, help='Training Epochs')
     parser.add_argument("--device", default="cuda", help="training device")
-    parser.add_argument('--lr', type=float, default=0.005, help='learning rate')
+    parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
     parser.add_argument('--weight_decay', type=float, default=0.001, help='l2 reg')
+    parser.add_argument('--num_hidden', type=int, default=64, help='Node dimension')
+    parser.add_argument('--num_channels', type=int, default=2, help='number of channels')
     parser.add_argument('--num_layers', type=int, default=2, help='number of layer')
     parser.add_argument('--norm', type=str, default='true', help='normalization')
     parser.add_argument('--scheduler_lr', type=bool, default=True, help='create learning rate scheduler')
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    device = torch.device(args.device)
+    edges, node_feature, labels = load_acm(args.data_path)
+
+
--- GTN/train_utils/train_eval.py
+++ GTN/train_utils/train_eval.py
@@ -9,9 +9,9 @@ from .optimizer_utils import create_lr_scheduler
 from .distributed_utils import Accumulator
 
 
-def train():
+def train(args):
     pass
 
 
-def test():
+def test(args):
     pass
