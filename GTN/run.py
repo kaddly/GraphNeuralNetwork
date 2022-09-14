68a2bfc7dd7028ef1a8bde9d31b922037ceb562b
cxz <1838038611@qq.com>
2022-09-14 08:30:19 +0800

    update

--- GTN/run.py
+++ GTN/run.py
@@ -8,7 +8,8 @@ def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="pytorch GTN training")
     parser.add_argument('--data_path', type=str, default='./data', help='DRIVE root')
-    parser.add_argument('--dataset', type=str, help='Dataset')
+    parser.add_argument('--model_dict_path', type=str, default='./saved_dict', help='Model Dict Saved Root')
+    parser.add_argument('--model', type=str, default='GTN')
     parser.add_argument('--epoch', type=int, default=40, help='Training Epochs')
     parser.add_argument("--device", default="cuda", help="training device")
     parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
@@ -16,7 +17,10 @@ def parse_args():
     parser.add_argument('--num_hidden', type=int, default=64, help='Node dimension')
     parser.add_argument('--num_channels', type=int, default=2, help='number of channels')
     parser.add_argument('--num_layers', type=int, default=2, help='number of layer')
-    parser.add_argument('--norm', type=str, default='true', help='normalization')
+    parser.add_argument('--norm', type=bool, default=True, help='normalization')
+    parser.add_argument('--is_current_train', type=bool, default=True, help='use current trained weight')
+    parser.add_argument('--print_freq', type=int, default=20, help='print val result frequent')
+    parser.add_argument('--adaptive_lr', type=bool, default=True, help='adaptive rate scheduler')
     parser.add_argument('--scheduler_lr', type=bool, default=True, help='create learning rate scheduler')
     return parser.parse_args()
 
@@ -27,4 +31,4 @@ if __name__ == '__main__':
     num_classes = torch.max(labels).item() + 1
     net = GTN_Model(A.shape[-1], args.num_channels, node_feature.shape[1], args.num_hidden, num_classes,
                     args.num_layers, args.norm)
-    train(net, args)
+    train(net, [A, node_feature, labels, train_idx, val_idx], args)
--- GTN/train_utils/train_eval.py
+++ GTN/train_utils/train_eval.py
@@ -9,8 +9,73 @@ from .optimizer_utils import create_lr_scheduler
 from .distributed_utils import Accumulator
 
 
-def train(net, args):
-    pass
+def train(net, data_iter, args):
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.xavier_uniform_(m.weight)
+
+    # 模型参数保存路径
+    parameter_path = os.path.join(args.model_dict_path, args.model)
+    if not os.path.exists(parameter_path):
+        os.mkdir(parameter_path)
+
+    if args.is_current_train and os.path.exists(os.path.join(parameter_path, 'HAN.ckpt')):
+        net.load_state_dict(torch.load(os.path.join(parameter_path, 'HAN.ckpt')), False)
+    else:
+        net.apply(init_weights)
+
+    device = torch.device(args.device) if torch.cuda.is_available() else torch.device('cpu')
+    net = net.to(device)
+    if args.adaptive_lr:
+        optimizer = torch.optim.Adam([{'params': net.weight},
+                                      {'params': net.linear1.parameters()},
+                                      {'params': net.linear2.parameters()},
+                                      {"params": net.layers.parameters(), "lr": 0.5}
+                                      ], lr=0.005, weight_decay=args.weight_decay)
+    else:
+        optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    if args.scheduler_lr:
+        lr_scheduler = create_lr_scheduler(optimizer, 1, args.epoch)
+
+    loss = nn.CrossEntropyLoss()
+    start_time = time.time()
+    dev_best_loss = float('inf')
+    last_improve = 0  # 记录上次验证集loss下降的batch数
+    metric = Accumulator(3)
+    A, node_feature, labels, train_idx, val_idx = [x.to(device) for x in data_iter]
+    num_classes = torch.max(labels).item() + 1
+    for epoch in range(args.epoch):
+        net.train()
+        optimizer.zero_grad()
+        y_hat, ws = net(A, node_feature, train_idx)
+        train_loss = loss(y_hat, labels[train_idx])
+        train_loss.backward()
+        optimizer.step()
+        if args.scheduler_lr:
+            lr_scheduler.step()
+        with torch.no_grad():
+            metric.add(train_loss.sum(), accuracy(y_hat, labels[train_idx]),
+                       f_beta_score(y_hat, labels[train_idx], num_classes))
+        if epoch % args.print_freq == 0:
+            net.eval()
+            lr_current = optimizer.param_groups[0]["lr"]
+            output = net(A, node_feature, val_idx)
+            val_loss = loss(output, labels[val_idx])
+            if dev_best_loss > val_loss:
+                torch.save(net.state_dict(), os.path.join(parameter_path, args.model + '.ckpt'))
+                dev_best_loss = val_loss
+                improve = '*'
+                last_improve = epoch
+            else:
+                improve = ''
+            time_dif = timedelta(seconds=int(round(time.time() - start_time)))
+            msg = 'Epoch [{0}/{1}]:  train_loss: {2:>5.3f},  train_acc: {3:>6.2%}, train_f1_score: {4:>5.3f},  train_lr: {5:>5.3f},  val_loss: {6:>5.3f}, val_acc: {7:>6.2%}, val_f1_score: {8:>5.3f}, Time: {9} {10}'
+            print(msg.format(epoch + 1, args.epoch, metric[0] / epoch, metric[1] / epoch, metric[2] / epoch, lr_current,
+                             val_loss.item(), accuracy(output, labels[val_idx]),
+                             f_beta_score(output, labels[val_idx], num_classes), time_dif, improve))
+        if epoch - last_improve > 5000:
+            print("No optimization for a long time, auto-stopping...")
+            break
 
 
 def test(net, args):
