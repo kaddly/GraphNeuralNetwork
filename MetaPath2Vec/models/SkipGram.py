571d06ebc3b376cce9529b81fd45e2f4326be7c7
cxz <1838038611@qq.com>
2022-09-21 07:59:48 +0800

    update

---
+++ MetaPath2Vec/models/SkipGram.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+from torch.nn import init
+
+
+class SkipGramModel(nn.Module):
+    def __init__(self, emb_size, emb_dimension, **kwargs):
+        """
+        :param emb_size:词表长度
+        :param emb_dimension:词向量长度
+        """
+        super(SkipGramModel, self).__init__(**kwargs)
+        self.emb_size = emb_size
+        self.emb_dimension = emb_dimension
+        self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+
+        initRange = 1.0 / self.emb_dimension
+        init.uniform_(self.u_embeddings.weight.data, -initRange, initRange)
+        init.constant_(self.v_embeddings.weight.data, 0)
+
+    def forward(self, center, contexts_and_negatives):
+        v = self.v_embeddings(center)
+        u = self.u_embeddings(contexts_and_negatives)
+        return torch.bmm(v, u.permute(0, 2, 1))
---
+++ MetaPath2Vec/utils/data_utils.py
@@ -0,0 +1,6 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+def read_wiki(data_dir='./data'):
+    pass
