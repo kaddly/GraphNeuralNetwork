20ee2060fffc6fc64df62afab7045b45fcda314d
cxz <1838038611@qq.com>
2022-09-24 08:42:22 +0800

    update

---
+++ MetaPath2Vec/utils/graph_utils.py
@@ -0,0 +1,10 @@
+import collections
+
+
+def procession_graph():
+    pass
+
+
+class HeteroGraph:
+    def __init__(self, gidx=[], ntypes=['_N'], etypes=['_E'], node_frames=None, edge_frames=None):
+        pass
