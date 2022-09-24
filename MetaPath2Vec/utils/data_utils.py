20176f95577961acde19dfcaf04837aa99d9112b
cxz <1838038611@qq.com>
2022-09-24 09:01:56 +0800

    update

--- MetaPath2Vec/utils/data_utils.py
+++ MetaPath2Vec/utils/data_utils.py
@@ -1,7 +1,11 @@
 import os
+import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+from .graph_utils import procession_graph
 
 
 def read_JData(data_dir=os.path.join('./', 'data')):
-    pass
+    edge_f = pd.read_csv(os.path.join(data_dir, 'data_action.csv'))
+    user_features = pd.read_csv(os.path.join(data_dir, 'user_features.csv'))
+    nodes_features = pd.read_csv(os.path.join(data_dir, 'item_features.csv'))
--- MetaPath2Vec/utils/graph_utils.py
+++ MetaPath2Vec/utils/graph_utils.py
@@ -1,8 +1,17 @@
 import collections
+import pandas as pd
 
 
-def procession_graph():
-    pass
+def procession_graph(edges: pd.DataFrame, sample_num=10000):
+    edges = edges.sample(sample_num)
+    users = set()
+    items = set()
+    for row in edges.iterrows():
+        users.add(row['user_id'])
+        items.add(row['sku_id'])
+    user_to_idx = {x: i for i, x in enumerate(users)}  # user编号
+    item_to_idx = {x: i for i, x in enumerate(items)}  # item编号
+    return list(users), user_to_idx, list(items), item_to_idx
 
 
 class HeteroGraph:
