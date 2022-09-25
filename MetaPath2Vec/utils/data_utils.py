877f1dfd6989801cc28292d1a208f993807137aa
cxz <1838038611@qq.com>
2022-09-25 08:28:50 +0800

    update

--- MetaPath2Vec/utils/data_utils.py
+++ MetaPath2Vec/utils/data_utils.py
@@ -2,10 +2,17 @@ import os
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
-from .graph_utils import procession_graph
+from .graph_utils import procession_graph, HeteroGraph
 
 
 def read_JData(data_dir=os.path.join('./', 'data')):
     edge_f = pd.read_csv(os.path.join(data_dir, 'data_action.csv'))
     user_features = pd.read_csv(os.path.join(data_dir, 'user_features.csv'))
     nodes_features = pd.read_csv(os.path.join(data_dir, 'item_features.csv'))
+    idx_to_users, user_to_idx, idx_to_items, item_to_idx = procession_graph(edge_f)
+    user_item_src = []
+    user_item_dst = []
+    for row in edge_f.iterrows():
+        user_item_src.append(user_to_idx.get(row['user_id']))
+        user_item_dst.append(item_to_idx.get(row['sku_id']))
+    HeteroGraph()
--- MetaPath2Vec/utils/graph_utils.py
+++ MetaPath2Vec/utils/graph_utils.py
@@ -1,5 +1,6 @@
 import collections
 import pandas as pd
+import networkx as nx
 
 
 def procession_graph(edges: pd.DataFrame, sample_num=10000):
@@ -14,6 +15,17 @@ def procession_graph(edges: pd.DataFrame, sample_num=10000):
     return list(users), user_to_idx, list(items), item_to_idx
 
 
-class HeteroGraph:
+class HeteroGraph(object):
     def __init__(self, gidx=[], ntypes=['_N'], etypes=['_E'], node_frames=None, edge_frames=None):
+        assert len(gidx) == len(ntypes)
+        self.graph_idx = gidx
+        self.node_types = ntypes
+        self.edge_types = etypes
+        self.node_features = node_frames
+        self.edge_features = edge_frames
+
+    def _relation_to_adj(self):
+        pass
+
+    def __repr__(self):
         pass
