6cc3eca055f63c4ebf3d23c27b0e1d928fff1fca
cxz <1838038611@qq.com>
2022-09-28 08:55:27 +0800

    update

--- MetaPath2Vec/utils/data_utils.py
+++ MetaPath2Vec/utils/data_utils.py
@@ -10,10 +10,14 @@ def read_JData(data_dir=os.path.join('../', 'data'), sample_num=10000):
     user_features = pd.read_csv(os.path.join(data_dir, 'user_features.csv'))
     nodes_features = pd.read_csv(os.path.join(data_dir, 'item_features.csv'))
     edge_f = edge_f.sample(sample_num)
+    user_features = user_features[user_features['user_id'].isin(list(edge_f['user_id']))]
+    nodes_features = nodes_features[nodes_features['user_id'].isin(list(edge_f['user_id']))]
     idx_to_users, user_to_idx, idx_to_items, item_to_idx = procession_graph(edge_f)
     user_item_src = [user_to_idx.get(user_id) for user_id in edge_f['user_id']]
     user_item_dst = [item_to_idx.get(item_id) for item_id in edge_f['sku_id']]
     HG = HeteroGraph([user_item_src, user_item_dst], edge_types=['user', 'item'], meta_path=['user', 'item', 'user'])
+    return HG, user_features, nodes_features, idx_to_users, user_to_idx, idx_to_items, item_to_idx
 
 
-read_JData()
+def load_JData(batch_size=128):
+    HG, user_features, nodes_features, idx_to_users, user_to_idx, idx_to_items, item_to_idx = read_JData()
