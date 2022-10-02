cf4ab66e876316ee425e13402dcc6e40a35e3488
cxz <1838038611@qq.com>
2022-10-02 08:28:48 +0800

    update

--- MetaPath2Vec/utils/data_utils.py
+++ MetaPath2Vec/utils/data_utils.py
@@ -5,7 +5,7 @@ import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 from utils.graph_utils import procession_graph, HeteroGraph, count_corpus
-from utils.sample_utils import RandomWalker
+from utils.sample_utils import RandomWalker, RandomGenerator
 
 
 def read_JData(data_dir=os.path.join('../', 'data'), sample_num=10000):
@@ -36,10 +36,52 @@ def subsample(sentences):
     return ([[token for token in line if keep(token)] for line in sentences], counter)
 
 
-def load_JData(batch_size=128):
+def get_centers_and_contexts(corpus, max_window_size):
+    """返回跳元模型中的中心词与上下文单词"""
+    centers, context = [], []
+    for line in corpus:
+        # 形成“中心词-上下文词”对，每个句子至少需要两个单词
+        if len(line) < 2:
+            continue
+        centers += line
+        for i in range(len(line)):
+            window_size = random.randint(1, max_window_size)
+            indices = list(range(max(0, i - window_size), min(len(line), i + 1 + window_size)))
+
+            # 从上下文词中排除中心词
+            indices.remove(i)
+            context.append([line[idx] for idx in indices])
+    return centers, context
+
+
+def get_negative(all_contexts, idx2node, counter, K):
+    """返回负采样中的噪声词"""
+    # 索引为1、2、...（索引0是词表中排除的未知标记）
+    sampling_weights = [counter[idx2node[i]] ** 0.75 for i in range(0, len(idx2node))]
+    all_negatives, generator = [], RandomGenerator(sampling_weights)
+    for contexts in all_contexts:
+        negatives = []
+        while len(negatives) < len(contexts) * K:
+            neg = generator.draw()
+            # 噪声词不能是上下⽂词
+            if neg not in contexts:
+                negatives.append(neg)
+        all_negatives.append(negatives)
+    return all_negatives
+
+
+def load_JData(batch_size=128, max_window_size=5, num_noise_words=2):
     HG, user_features, nodes_features, idx_to_users, user_to_idx, idx_to_items, item_to_idx = read_JData()
     generator = RandomWalker(HG)
-    contexts = generator.simulate_walks(num_walks=10, meta_path=['user', 'item', 'user', 'item', 'user'], workers=2)
+    all_contexts = generator.simulate_walks(num_walks=10, meta_path=['user', 'item', 'user', 'item', 'user'], workers=2)
+    print('load contexts:'+str(len(all_contexts)))
+    subsampled, counter = subsample(all_contexts)
+    print('load subsampled contexts:' + str(len(subsampled)))
+    corpus = [[node2idx[token] for token in line] for line in subsampled]
+    all_centers, all_contexts = get_centers_and_contexts(corpus, max_window_size)
+    print('load all_centers:'+str(len(all_centers)))
+    all_negatives = get_negative(all_contexts, idx2node, counter, num_noise_words)
+    print('load all_negatives:' + str(len(all_negatives)))
 
 
 load_JData()
--- MetaPath2Vec/utils/sample_utils.py
+++ MetaPath2Vec/utils/sample_utils.py
@@ -43,3 +43,22 @@ class RandomWalker:
             for v in nodes:
                 walks.append(self.meta_path_walk(meta_path=meta_path, start_node=v))
         return walks
+
+
+class RandomGenerator:
+    """根据n个采样权重在{1,...,n}中随机抽取"""
+
+    def __init__(self, sampling_weights):
+        # Exclude
+        self.population = list(range(0, len(sampling_weights)))
+        self.sampling_weights = sampling_weights
+        self.candidates = []
+        self.i = 0
+
+    def draw(self):
+        if self.i == len(self.candidates):
+            # 缓存k个随机采样结果
+            self.candidates = random.choices(self.population, self.sampling_weights, k=10000)
+            self.i = 0
+        self.i += 1
+        return self.candidates[self.i - 1]
