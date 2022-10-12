76e883d75e6c7e83e4b432424fccb7cf6c7bf50d
cxz <1838038611@qq.com>
2022-10-12 08:37:55 +0800

    update

--- GATNE/utils/data_utils.py
+++ GATNE/utils/data_utils.py
@@ -6,9 +6,45 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 
 
-def read_data(data_dir=os.path.join('./', 'data'), dataset='amazon'):
-    with open(data_dir, 'r') as f:
-        pass
+def read_train_data(data_dir=os.path.join('./', 'data'), dataset='amazon'):
+    data_path = os.path.join(data_dir, dataset)
+    edge_data_by_type = dict()  # 每个type对应到的相连接节点
+    all_nodes = list()  # 所有节点的集合
+    print("We are loading data from:" + dataset + 'train.txt')
+    with open(os.path.join(data_path, 'train_txt'), 'r') as f:
+        for line in f:
+            words = line[:-1].split(" ")
+            if words[0] not in edge_data_by_type:  # edge type涉及到的节点
+                edge_data_by_type[words[0]] = list()
+            x, y = words[1], words[2]
+            edge_data_by_type[words[0]].append((x, y))
+            all_nodes.append(x)
+            all_nodes.append(y)
+    all_nodes = list(set(all_nodes))  # nodes去重
+    print('Total training nodes: ' + str(len(all_nodes)))
+    return edge_data_by_type  # 每个type连接的点边情况
 
 
+def read_test_data(data_dir=os.path.join('./', 'data'), dataset='amazon'):
+    data_path = os.path.join(data_dir, dataset)
+    true_edge_data_by_type = dict()  # true样本
+    false_edge_data_by_type = dict()  # false样本
+    all_nodes = list()
+    print("We are loading data from:" + dataset + 'test.txt')
+    with open(os.path.join(data_path, 'test.txt'), 'r') as f:
+        for line in f:
+            words = line[:-1].split(' ')
+            x, y = words[1], words[2]
+            if int(words[3]) == 1:  # true对应到的节点
+                if words[0] not in true_edge_data_by_type:
+                    true_edge_data_by_type[words[0]] = list()  # true对应到的type相连接节点
+                true_edge_data_by_type[words[0]].append((x, y))
+            else:
+                if words[0] not in false_edge_data_by_type:
+                    false_edge_data_by_type[words[0]] = list()
+                false_edge_data_by_type[words[0]].append((x, y))
+            all_nodes.append(x)
+            all_nodes.append(y)
+    all_nodes = list(set(all_nodes))
+    return true_edge_data_by_type, false_edge_data_by_type
 
