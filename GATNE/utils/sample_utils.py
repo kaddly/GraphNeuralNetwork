3b0999b9260fda0ec4f7ab107f247f3009adba0e
cxz <1838038611@qq.com>
2022-10-15 08:17:04 +0800

    update

--- GATNE/utils/sample_utils.py
+++ GATNE/utils/sample_utils.py
@@ -4,29 +4,6 @@ import tqdm
 import itertools
 
 
-def walk(args):
-    walk_length, start, schema = args
-    # Simulate a random walk starting from start node.
-    rand = random.Random()
-
-    if schema:
-        schema_items = schema.split('-')
-        assert schema_items[0] == schema_items[-1]  # metapath前后一致; A-B-A
-
-    walk = [start]
-    while len(walk) < walk_length:
-        cur = walk[-1]  # 当前节点
-        candidates = []
-        for node in G[cur]:  # 和cur节点相连接的节点; 候选节点
-            if schema == '' or node_type[node] == schema_items[len(walk) % (len(schema_items) - 1)]:
-                candidates.append(node)
-        if candidates:
-            walk.append(rand.choice(candidates))
-        else:
-            break
-    return [str(node) for node in walk]
-
-
 class RWGraph:
     def __init__(self, nx_G, node_type_arr=None, num_workers=16):
         self.G = nx_G
@@ -45,15 +22,37 @@ class RWGraph:
 
         if schema is None:
             results = Parallel(n_jobs=workers, verbose=verbose)(
-                delayed(walk)(walk_length, node, '') for node in tqdm(self.node_list(nodes, num_walks)))
+                delayed(self.walk)(walk_length, node, '') for node in tqdm(self.node_list(nodes, num_walks)))
             all_walks = list(itertools.chain(*results))
         else:
             schema_list = schema.split(',')
             for schema_iter in schema_list:
                 results = Parallel(n_jobs=workers, verbose=verbose)(
-                    delayed(walk)(walk_length, node, schema_iter) for node in tqdm(self.node_list(nodes, num_walks)) if
+                    delayed(self.walk)(walk_length, node, schema_iter) for node in tqdm(self.node_list(nodes, num_walks)) if
                     schema_iter.split('-')[0] == self.node_type[node])
                 walks = list(itertools.chain(*results))
                 all_walks.extend(walks)
 
         return all_walks
+
+    def walk(self, args):
+        walk_length, start, schema = args
+        # Simulate a random walk starting from start node.
+        rand = random.Random()
+
+        if schema:
+            schema_items = schema.split('-')
+            assert schema_items[0] == schema_items[-1]  # metapath前后一致; A-B-A
+
+        walk = [start]
+        while len(walk) < walk_length:
+            cur = walk[-1]  # 当前节点
+            candidates = []
+            for node in self.G[cur]:  # 和cur节点相连接的节点; 候选节点
+                if schema == '' or self.node_type[node] == schema_items[len(walk) % (len(schema_items) - 1)]:
+                    candidates.append(node)
+            if candidates:
+                walk.append(rand.choice(candidates))
+            else:
+                break
+        return [str(node) for node in walk]
