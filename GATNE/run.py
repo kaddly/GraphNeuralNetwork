2e394ce7e56a4f9e4fb56dcdda249b1e9a409d6c
cxz <1838038611@qq.com>
2022-10-29 09:16:32 +0800

    update

--- GATNE/run.py
+++ GATNE/run.py
@@ -6,34 +6,48 @@ from train_utils import train, NSLoss
 
 def parse_args():
     import argparse
-    parser = argparse.ArgumentParser(description="pytorch MetaPath2vec training")
+    parser = argparse.ArgumentParser(description="pytorch GATNE training")
+    # data process parameter
     parser.add_argument('--data_dir', type=str, default=os.path.join(os.path.abspath('.'), 'data'), help='DRIVE root')
     parser.add_argument('--dataset', type=str, default='amazon', help='Dataset name')
-    parser.add_argument('--model_dict_path', type=str, default='./saved_dict', help='Model Dict Saved Root')
-    parser.add_argument('--model', type=str, default='MetaPath2vec')
-    parser.add_argument('--num_epoch', type=int, default=100, help='Training Epochs')
-    parser.add_argument('--num_batch', type=int, default=512, help='Training Batches')
-    parser.add_argument('--schema', type=list, default=None)
-    parser.add_argument('--walk-length', type=int, default=10,
+    parser.add_argument('--schema', type=str, default=None,
+                        help='The MetaPath schema (e.g., U-I-U,I-U-I).')
+    parser.add_argument('--walk_length', type=int, default=10,
                         help='Length of walk per source. Default is 10.')
 
-    parser.add_argument('--num-walks', type=int, default=10,
+    parser.add_argument('--num_walks', type=int, default=10,
                         help='Number of walks per source. Default is 20.')
 
-    parser.add_argument('--window-size', type=int, default=5,
+    parser.add_argument('--window_size', type=int, default=5,
                         help='Context size for optimization. Default is 5.')
 
-    parser.add_argument('--negative-samples', type=int, default=5,
+    parser.add_argument('--negative_samples', type=int, default=5,
                         help='Negative samples for optimization. Default is 5.')
 
-    parser.add_argument('--neighbor-samples', type=int, default=10,
+    parser.add_argument('--neighbor_samples', type=int, default=10,
                         help='Neighbor samples for aggregation. Default is 10.')
-    parser.add_argument('--num-workers', type=int, default=4,
+    parser.add_argument('--num_workers', type=int, default=4,
                         help='Number of workers for generating random walks. Default is 16.')
+
+    # model parameter
+    parser.add_argument('--dimensions', type=int, default=256,
+                        help='Number of dimensions. Default is 256.')
+
+    parser.add_argument('--edge_dim', type=int, default=16,
+                        help='Number of edge embedding dimensions. Default is 16.')
+
+    parser.add_argument('--att_dim', type=int, default=32,
+                        help='Number of attention dimensions. Default is 32.')
+    # train parameter
+    parser.add_argument('--model_dict_path', type=str, default='./saved_dict', help='Model Dict Saved Root')
+    parser.add_argument('--model', type=str, default='GATNE')
+    parser.add_argument('--num_epoch', type=int, default=100, help='Training Epochs')
+    parser.add_argument('--num_batch', type=int, default=512, help='Training Batches')
+    parser.add_argument('--eval_type', type=str, default='all',
+                        help='The edge type(s) for evaluation.')
     parser.add_argument("--device", default="cuda", help="training device")
     parser.add_argument('--lr', type=float, default=0.4, help='learning rate')
     parser.add_argument('--weight_decay', type=float, default=0.001, help='l2 reg')
-    parser.add_argument('--num_hidden', type=int, default=128, help='Node dimension')
     parser.add_argument('--is_current_train', type=bool, default=True, help='use current trained weight')
     parser.add_argument('--print_freq', type=int, default=20, help='print val result frequent')
     parser.add_argument('--Max_auto_stop_epoch', type=int, default=5000)
@@ -45,7 +59,6 @@ if __name__ == '__main__':
     args = parse_args()
     train_iter, vocab, training_data_by_type, valid_true_data_by_edge, valid_false_data_by_edge, testing_true_data_by_edge, testing_false_data_by_edge, features = load_data(
         args)
-    net = GATNEModel(len(vocab), args.embedding_size, args.embedding_u_size, len(training_data_by_type), args.dim_a,
-                     features)
-    loss = NSLoss(len(vocab), args.negative_samples, args.embedding_size)
+    net = GATNEModel(len(vocab), args.dimensions, args.edge_dim, len(training_data_by_type), args.dim_a, features)
+    loss = NSLoss(len(vocab), args.negative_samples, args.dimensions)
     train(net, loss, train_iter, (valid_true_data_by_edge, valid_false_data_by_edge), args)
--- GATNE/train_utils/train_eval.py
+++ GATNE/train_utils/train_eval.py
@@ -10,4 +10,24 @@ from .distributed_utils import Accumulator
 
 
 def train(net, loss, train_iter, val_iter, args):
-    pass
+    # 模型参数保存路径
+    parameter_path = os.path.join(args.model_dict_path, args.model)
+    if not os.path.exists(parameter_path):
+        os.mkdir(parameter_path)
+
+    if args.is_current_train and os.path.exists(os.path.join(parameter_path, args.model + '.ckpt')):
+        net.load_state_dict(torch.load(os.path.join(parameter_path, args.model + '.ckpt')), False)
+
+    device = torch.device(args.device) if torch.cuda.is_available() else torch.device('cpu')
+    net = net.to(device)
+    loss = loss.to(device)
+    optimizer = torch.optim.SGD([{"params": net.parameters()}, {"params": loss.parameters()}], lr=args.lr, weight_decay=args.weight_decay)
+    if args.scheduler_lr:
+        lr_scheduler = create_lr_scheduler(optimizer, args.num_batch, args.num_epoch)
+
+    start_time = time.time()
+    total_batch = 0  # 记录进行到多少batch
+    best_loss = float('inf')
+    last_improve = 0  # 记录上次验证集loss下降的batch数
+    flag = False  # 记录是否很久没有效果提升
+    metric = Accumulator(5)
