7c7b6bc520417495fb3884e4153f46bedab78196
cxz <1838038611@qq.com>
2022-11-20 09:02:06 +0800

    update

--- GATNE_Pytorch/utils/data_utils.py
+++ GATNE_Pytorch/utils/data_utils.py
@@ -3,8 +3,12 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 
 
-def read_data(data_dir=os.path.join(os.path.abspath('.'), 'data')):
-    pass
+def read_data(data_dir=os.path.join(os.path.abspath('.'), 'data'), dataset='amazon'):
+    train_data_path = os.path.join(data_dir, dataset, 'train.txt')
+    val_data_path = os.path.join(data_dir, dataset, 'valid.txt')
+    test_data_path = os.path.join(data_dir, dataset, 'test.txt')
+    edge_data_by_type = dict()  # 每个type对应到的相连接节点
+    all_nodes = list()  # 所有节点的
 
 
 class MyDataset(Dataset):
