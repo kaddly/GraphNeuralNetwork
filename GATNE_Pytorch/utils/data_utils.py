1c74b116d410875eb4f145f17483f694bd7c9210
cxz <1838038611@qq.com>
2022-11-22 09:13:27 +0800

    update

--- GATNE_Pytorch/utils/data_utils.py
+++ GATNE_Pytorch/utils/data_utils.py
@@ -22,8 +22,29 @@ def read_data(data_dir=os.path.join(os.path.abspath('.'), 'data'), dataset='amaz
     all_nodes = list(set(all_nodes))  # nodes去重
     print('Total training nodes: ' + str(len(all_nodes)))
 
+    def process_val_data(f_index):
+        true_edge_data_by_type = dict()  # true样本
+        false_edge_data_by_type = dict()  # false样本
+        for sentence in f_index:
+            tokens = sentence[:-1].split(' ')
+            x, y = tokens[0], tokens[1]
+            if int(tokens[3]) == 1:  # true对应到的节点
+                if words[0] not in true_edge_data_by_type:
+                    true_edge_data_by_type[tokens[0]] = list()  # true对应到的type相连接节点
+                true_edge_data_by_type[tokens[0]].append((x, y))
+            else:
+                if tokens[0] not in false_edge_data_by_type:
+                    false_edge_data_by_type[tokens[0]] = list()
+                false_edge_data_by_type[tokens[0]].append((x, y))
+        return true_edge_data_by_type, false_edge_data_by_type
+
     with open(val_data_path, 'r') as f:
-        pass
+        val_true_edge_data_by_type, val_false_edge_data_by_type = process_val_data(f)
+
+    with open(test_data_path, 'r') as f:
+        test_true_edge_data_by_type, test_false_edge_data_by_type = process_val_data(f)
+
+    return edge_data_by_type, val_true_edge_data_by_type, val_false_edge_data_by_type, test_true_edge_data_by_type, test_false_edge_data_by_type
 
 
 class MyDataset(Dataset):
