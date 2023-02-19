f9c83fef7e1f3b62f4468f1b8de4bb631d9fe9f3
cxz <1838038611@qq.com>
2023-02-19 15:47:23 +0800

    update

--- BiNE/utils/data_utils.py
+++ BiNE/utils/data_utils.py
@@ -1,6 +1,5 @@
 import os
 import random
-import math
 import torch
 from torch.utils.data import DataLoader, Dataset
 from utils.graph_utils import BipartiteGraph
@@ -28,57 +27,113 @@ def generator_walks(meta_path, BG: BipartiteGraph, vocab, maxT, minT, percentage
 
 def get_centers_and_contexts(corpus, max_window_size):
     """返回跳元模型中的中心词与上下文单词"""
-    centers, context = [], []
+    context = {}
     for line in corpus:
         # 形成“中心词-上下文词”对，每个句子至少需要两个单词
         if len(line) < 2:
             continue
-        centers += line
         for i in range(len(line)):
+            if i not in context:
+                context[i] = []
             window_size = random.randint(1, max_window_size)
             indices = list(range(max(0, i - window_size), min(len(line), i + 1 + window_size)))
-
             # 从上下文词中排除中心词
             indices.remove(i)
-            context.append([line[idx] for idx in indices])
-    return centers, context
+            context[i].append([line[idx] for idx in indices])
+    return context
 
 
-def get_negative(all_contexts, vocab, K):
+def get_negative(contexts, vocab, K):
     """返回负采样中的噪声词"""
     # 索引为1、2、...（索引0是词表中排除的未知标记）
     sampling_weights = [vocab.token_counter[vocab.to_tokens(i)] ** 0.75 for i in range(1, len(vocab))]
-    all_negatives, generator = [], RandomGenerator(sampling_weights)
-    for contexts in all_contexts:
+    all_negatives, generator = {}, RandomGenerator(sampling_weights)
+    for center, context in contexts:
+        if center not in all_negatives:
+            all_negatives[center] = []
         negatives = []
-        while len(negatives) < len(contexts) * K:
+        while len(negatives) < len(context[len(all_negatives[center])]) * K:
             neg = generator.draw()
             # 噪声词不能是上下⽂词
-            if neg not in contexts:
+            if neg not in context[len(all_negatives[center])]:
                 negatives.append(neg)
-        all_negatives.append(negatives)
+        all_negatives[center].append(negatives)
     return all_negatives
 
 
-def generator_explicit_relations():
-    pass
-
-
 def generator_implicit_relations(corpus, vocab, max_window_size, K):
-    centers, all_contexts = get_centers_and_contexts(corpus, max_window_size)
+    all_contexts = get_centers_and_contexts(corpus, max_window_size)
     all_negatives = get_negative(all_contexts, vocab, K)
-    return centers, all_contexts, all_negatives
+    return all_contexts, all_negatives
 
 
 class BipartiteDataset(Dataset):
-    def __init__(self, user_centers, user_contexts, user_negatives, item_centers, item_contexts, item_negatives):
-        assert len(user_centers) == len(user_contexts)
+    def __init__(self, u, v, w):
+        assert len(u) == len(v) == len(w)
+        self.u = u
+        self.v = v
+        self.w = w
 
     def __len__(self):
-        pass
+        return len(self.w)
 
     def __getitem__(self, item):
-        pass
+        return self.w[item], self.v[item], self.w[item]
+
+
+def generator_contexts_negatives_mask_label(contexts, negatives, max_len):
+    contexts_negatives, masks, labels = [], [], []
+    for context, negative in zip(contexts, negatives):
+        cur_len = len(context) + len(negative)
+        contexts_negatives += [context + negative + [0] * (max_len - cur_len)]
+        masks += [[1] * cur_len + [0] * (max_len - cur_len)]
+        labels += [[1] * len(context) + [0] * (max_len - len(context))]
+    return contexts_negatives, masks, labels
+
+
+class Collate_fn:
+    def __init__(self, u_c, u_n, i_c, i_n):
+        self.u_c = u_c
+        self.u_n = u_n
+        self.i_c = i_c
+        self.i_n = i_n
+
+    def __call__(self, data):
+        batch_max_u, batch_max_i = 0, 0
+        for u, i, _ in data:
+            u_contexts = self.u_c[u]
+            u_negatives = self.u_n[u]
+            max_u = max(len(u_context) + len(u_negative) for u_context, u_negative in zip(u_contexts, u_negatives))
+            if max_u > batch_max_u:
+                batch_max_u = max_u
+            i_contexts = self.i_c[u]
+            i_negatives = self.i_n[u]
+            max_i = max(len(i_context) + len(i_negative) for i_context, i_negative in zip(i_contexts, i_negatives))
+            if max_i > batch_max_i:
+                batch_max_i = max_i
+        users, items, weights, user_contexts_negatives, item_contexts_negatives, user_masks, item_masks, user_labels, item_labels = [], [], [], [], [], [], [], [], []
+        for u, i, w in data:
+            users += u
+            items += i
+            weights += w
+            contexts_negatives, masks, labels = generator_contexts_negatives_mask_label(self.u_c[u], self.u_n[u],
+                                                                                        batch_max_u)
+            user_contexts_negatives.append(contexts_negatives)
+            user_masks.append(masks)
+            user_labels.append(labels)
+            contexts_negatives, masks, labels = generator_contexts_negatives_mask_label(self.i_c[i], self.i_n[i],
+                                                                                        batch_max_i)
+            item_contexts_negatives.append(contexts_negatives)
+            item_masks.append(masks)
+            item_labels.append(labels)
+        return (torch.tensor(users), torch.tensor(items), torch.Tensor(weights),
+                torch.tensor(user_contexts_negatives), torch.tensor(user_masks), torch.tensor(user_labels),
+                torch.tensor(item_contexts_negatives), torch.tensor(item_masks), torch.tensor(item_labels))
+
+
+def readTestDataset(data_set, file_name):
+    [users, items], labels = read_data(data_set, file_name)
+    return torch.tensor(users), torch.tensor(items), torch.tensor(labels)
 
 
 def load_data(args):
@@ -91,7 +146,10 @@ def load_data(args):
                                   percentage=args.percentage, hits_dict=u_hits_dict)
     item_corpus = generator_walks(meta_path=['I', 'U', 'I'], BG=BG, vocab=item_vocab, maxT=args.maxT, minT=args.minT,
                                   percentage=args.percentage, hits_dict=i_hits_dict)
-    user_centers, user_contexts, user_negatives = generator_implicit_relations(user_corpus, user_vocab,
-                                                                               args.max_window_size, args.K)
-    item_centers, item_contexts, item_negatives = generator_implicit_relations(item_corpus, item_vocab,
-                                                                               args.max_window_size, args.K)
+    user_contexts, user_negatives = generator_implicit_relations(user_corpus, user_vocab, args.max_window_size, args.K)
+    item_contexts, item_negatives = generator_implicit_relations(item_corpus, item_vocab, args.max_window_size, args.K)
+    train_dataset = BipartiteDataset(user_vocab[relation_list[0]], item_vocab[relation_list[1]], weights_list)
+    collate_fn = Collate_fn(user_contexts, user_negatives, item_contexts, item_negatives)
+    train_iter = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
+    testDataset = readTestDataset(args.data_set, args.test_file_name)
+    return train_iter, testDataset
